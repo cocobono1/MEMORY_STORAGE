c3bb8868f11988c711402dd9d1db6458a15a448a
mmmmmmmmmmmuk <147673787+mmmmmmmmmmmuk@users.noreply.github.com>
2023-10-13 18:00:56 +0900

    .1

---
+++ Java02_Variable/src/edu/java/variable10/ScannerMain03.java
@@ -0,0 +1,19 @@
+package edu.java.variable10;
+
+import java.util.Scanner;
+
+public class ScannerMain03 {
+
+	public static void main(String[] args) {
+		System.out.println("문자 하나 입력");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0); //문자 하나 입력 후 저장
+		System.out.println("ch = " + ch);
+		
+		sc.close();
+		
+	}// endMain()
+
+}// endSannerMain03
